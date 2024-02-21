e136c45277894423462032b03e05b451f18a68fa
Don-Cod <masyome@gmail.com>
2024-02-21 12:25:32 -0800

    MySQL_Advanced

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,12 @@
+-- A function SafeDiv that dividesand returns
+-- the first by the second number or returns 0
+
+DELIMITER $$
+DROP FUNCTION IF EXISTS SafeDiv;
+CREATE FUNCTION SafeDiv(a INT, b INT)
+RETURNS FLOAT
+BEGIN
+    RETURN (IF (b = 0, 0, a / b));
+END
+$$
+DELIMITER ;
